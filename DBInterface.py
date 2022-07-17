66e4f95bf9809cd881d5750f15cfbc05eed7a3bc
Paul Dante <paul.m.dante@gmail.com>
2022-07-17 14:20:09 -0700

    db now sets up sqlite db

--- DBInterface.py
+++ DBInterface.py
@@ -1,8 +1,18 @@
 import sqlite3
 
+
 class DBInterface:
     def __init__(self):
         self.con = sqlite3.connect('data/birddb.db')
         self.con.row_factory = sqlite3.Row
         self.cur = self.con.cursor()
-        
+
+    def setup(self):
+        pathToSql = 'sql/birdsql.sql'
+        with open(pathToSql, 'r') as scriptfile:
+            contents = scriptfile.read()
+            self.cur.executescript(contents)
+
+
+d = DBInterface()
+d.setup()
